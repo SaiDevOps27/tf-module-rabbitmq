2c0feecc3a619899c2c5b4e5158e54178931c0bc
saidevops7722 <saiaws7722@gmail.com>
2024-04-27 20:01:37 +0530

    jenkins provider info

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex = "devops-practice-with-ansible"
+#  owners = ["self"]
+#}
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex = "devops-practice-with-ansible"
-  owners = ["self"]
+  name_regex = "Centos-8-Devops-Practice"
+  owners = ["792438259559"]
 }
 
 data "aws_route53_zone" "domain" {
--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/SaiDevOps27/roboshop-ansible.git roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
 
