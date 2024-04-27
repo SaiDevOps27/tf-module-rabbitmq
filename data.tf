55d1c100a2425e5da68301d5cedd76c6541ee115
saidevops7722 <saiaws7722@gmail.com>
2024-04-27 20:32:27 +0530

    jenkins provider info

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@
 data "aws_ami" "ami" {
   most_recent = true
   name_regex = "Centos-8-Devops-Practice"
-  owners = ["792438259559"]
+  owners = ["973714476881"]
 }
 
 data "aws_route53_zone" "domain" {
